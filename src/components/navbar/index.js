cc0cb9196c1a565670933d141675ebafa99b6884
Gonzalo Núñez <92396308+Gonzalonu99@users.noreply.github.com>
2022-10-06 16:41:43 -0300

    feat navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Nav, Navbar, NavbarBrand, NavLink } from 'react-bootstrap';
-import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
-import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
-import Logo from './logo.png';
-import './navbar.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Nav, Navbar, NavbarBrand, NavLink } from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
+import Logo from "./newlogo365.png";
+import "./navbar.css";
 
 const NavBar = () => {
   return (
     <div>
-        <Navbar bg='myColor' variant='light' sticky='top' expand='md' collapseOnSelect>
-            <NavbarBrand><img src={Logo} alt="Autoshop-logo" className='navbar-logo'/></NavbarBrand>
-            <NavbarToggle/>
-            <NavbarCollapse>
-                <Nav>
-                    <NavLink href='#home'>Home</NavLink>
-                    <NavLink href='#about'>Nosotros</NavLink>
-                    <NavLink href='#offices'>Sucursales</NavLink>
-                    <NavLink className='contact-button' href='#contact'>Contáctanos</NavLink>
-                </Nav>
-            </NavbarCollapse>
-        </Navbar>
+      <Navbar
+        bg="myColor"
+        variant="light"
+        sticky="top"
+        expand="md"
+        collapseOnSelect
+      >
+        <NavbarBrand>
+          <img src={Logo} alt="Autoshop-logo" className="navbar-logo" />
+        </NavbarBrand>
+        <NavbarToggle />
+        <NavbarCollapse>
+          <Nav>
+            <NavLink href="#offices">Sucursales</NavLink>
+            <NavLink className="contact-button" href="#contact">
+              Contáctanos
+            </NavLink>
+          </Nav>
+        </NavbarCollapse>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
