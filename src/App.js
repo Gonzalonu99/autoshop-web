5f30f781a7744566b1ca63b8d99f93f8fff4e0cf
Gonzalo Núñez <92396308+Gonzalonu99@users.noreply.github.com>
2022-11-15 18:54:03 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { Fragment } from "react";
+import Descargar from "./components/descargar";
+import Form from "./components/formulario";
 import Home from "./components/home";
+import Integrar from "./components/integrar";
 import NavBar from "./components/navbar";
+import ScrollUp from "./components/scroll";
+import Sucursales from "./components/sucursales";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <Home/>
-    </div>
+    <Fragment>
+      <NavBar />
+      <Home />
+      <Integrar />
+      <Descargar />
+      <Sucursales />
+      <Form />
+      <ScrollUp />
+    </Fragment>
   );
 }
 
