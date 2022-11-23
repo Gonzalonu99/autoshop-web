8b1ed8a0a45a8cfda624d19cd2b3955cc7cabf03
Gonzalo Núñez <92396308+Gonzalonu99@users.noreply.github.com>
2022-11-23 12:50:34 -0300

    feat: footer icons

---
+++ src/components/footer/LinkedInIcon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
+const LinkedInIcon = () => {
+  return (
+    <FontAwesomeIcon icon={faLinkedin}/>
+  )
+}
+
+export default LinkedInIcon
